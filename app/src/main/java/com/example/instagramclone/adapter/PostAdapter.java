4b8968c627e2ea52d3c55b36786654576d744b12
Gulcihan <gulmezgulcihan44@gmail.com>
2024-02-27 11:24:32 +0300

    initial commit

---
+++ app/src/main/java/com/example/instagramclone/adapter/PostAdapter.java
@@ -0,0 +1,54 @@
+package com.example.instagramclone.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.instagramclone.databinding.RecyclerRowBinding;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostHolder> {
+
+    public PostAdapter(ArrayList<Post> postArrayList) {
+        this.postArrayList = postArrayList;
+    }
+
+    private ArrayList<Post> postArrayList;
+
+
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()) , parent , false);
+        return new PostHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+        holder.recyclerRowBinding.recyclerViewUserEmailText.setText(postArrayList.get(position).email);
+        holder.recyclerRowBinding.recyclerViewCommentText.setText(postArrayList.get(position).comment);
+        Picasso.get().load(postArrayList.get(position).downloadurl).into(holder.recyclerRowBinding.recyclerViewImageView);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return postArrayList.size();
+    }
+
+    static class PostHolder extends RecyclerView.ViewHolder{
+
+        RecyclerRowBinding recyclerRowBinding;
+
+        public PostHolder(RecyclerRowBinding recyclerRowBinding) {
+            super(recyclerRowBinding.getRoot());
+            this.recyclerRowBinding = recyclerRowBinding;
+        }
+    }
+
+}
